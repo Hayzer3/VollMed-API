635c04a39534ac2c22af2ebc8a2aeff620ba3fc4
Hayzer3 <lucashyzr@gmail.com>
2026-02-17 10:41:52 -0300

    feat: fix patient entity and add oracle migration

--- src/main/java/med/voli/api/controller/PacienteController.java
+++ src/main/java/med/voli/api/controller/PacienteController.java
@@ -1,15 +1,27 @@
 package med.voli.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voli.api.medico.Medico;
 import med.voli.api.paciente.DadosCadastroPaciente;
+import med.voli.api.paciente.Paciente;
+import med.voli.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("pacientes")
 public class PacienteController {
 
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados){
-        System.out.println(dados);
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 }
