9339ae0ef1296c7240440d5f30fe0b548f10ae66
Hayzer3 <lucashyzr@gmail.com>
2026-02-27 15:29:01 -0300

    feat: implementar listagem de médicos

--- src/main/java/med/voli/api/controller/MedicoController.java
+++ src/main/java/med/voli/api/controller/MedicoController.java
@@ -3,13 +3,13 @@ package med.voli.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voli.api.medico.DadosCadastroMedico;
+import med.voli.api.medico.DadosListagemMedico;
 import med.voli.api.medico.Medico;
 import med.voli.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,4 +23,10 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
+
 }
